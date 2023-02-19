e57c742a989ce5a7d54246c2700cdd68c607f4a8
ASHU-code06 <ashuc306@gmail.com>
2023-02-20 03:33:21 +0530

    Measured length of two lines and compared them

--- LineComparision.java
+++ LineComparision.java
@@ -15,7 +15,7 @@ public class LineComparision {
 
         double len1 ;
         len1=Math.sqrt(Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)))  ;
-        System.out.println("length of line 1 is   "+len1);
+        System.out.println("length of line 1 is   "+len1+"\n");
 
         System.out.println("Enter x1 and y1 co-ordinate for line 2 ");
         double a1 = myinput.nextDouble();
@@ -27,13 +27,14 @@ public class LineComparision {
 
         double len2 ;
         len2=Math.sqrt(Math.pow((a2-a1),2)+Math.pow((b2-b1),2)) ;
-        System.out.println("length of line 2 is   "+len2);
+        System.out.println("length of line 2 is   "+len2+"\n");
 
         if(len1==len2){
             System.out.println("The lines are equal");
-        }
-        else {
-            System.out.println("The lines are not equal");
+        } else if (len1>len2) {
+            System.out.println("The line1 is greater");
+        } else if (len2>len1) {
+            System.out.println("The line2 is greater");
         }
     }
 }
